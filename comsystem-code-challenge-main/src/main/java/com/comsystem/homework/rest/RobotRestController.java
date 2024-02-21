b2198b5e1b8bfb5d0a9d61d973af2882a12a1c63
prdimitrov <122304481+prdimitrov@users.noreply.github.com>
2024-02-21 12:52:30 +0200

    Add files via upload

---
+++ comsystem-code-challenge-main/src/main/java/com/comsystem/homework/rest/RobotRestController.java
@@ -0,0 +1,38 @@
+package com.comsystem.homework.rest;
+
+import com.comsystem.homework.model.RobotPlan;
+import com.comsystem.homework.robot.RobotOperations;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/api/v1/robot/operation")
+public final class RobotRestController {
+    //Calling the RobotOperations class.
+    public RobotRestController(RobotOperations robotOperations) {
+        this.robotOperations = robotOperations;
+    }
+
+    RobotOperations robotOperations;
+
+    /**
+     * This method exposes the functionality of {@link RobotOperations#excavateStonesForDays(int)} via HTTP
+     */
+    @PostMapping("/excavation")
+    public ResponseEntity<RobotPlan> excavateStones(@RequestParam Integer numberOfDays) {
+        RobotPlan robotPlan = robotOperations.excavateStonesForDays(numberOfDays);
+        return ResponseEntity.ok(robotPlan);
+    }
+
+    /**
+     * This method exposes the functionality of {@link RobotOperations#daysRequiredToCollectStones(int)} via HTTP
+     */
+    @PostMapping("/approximation")
+    public ResponseEntity<RobotPlan> approximateDays(@RequestParam Integer numberOfStones) {
+        RobotPlan robotPlan = robotOperations.daysRequiredToCollectStones(numberOfStones);
+        return ResponseEntity.ok(robotPlan);
+    }
+}
